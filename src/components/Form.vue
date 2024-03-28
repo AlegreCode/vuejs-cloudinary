97005b2834c9b1099b396c0faf96b67a368eacfe
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-03-28 17:20:04 -0300

    Creamos el componente Form.vue
    Creamos un formulario con un campo de tipo file para cargar la imagen y un elemento img que será la vista previa.

---
+++ src/components/Form.vue
@@ -0,0 +1,11 @@
+<template>
+    <form>
+      <div class="form-group py-2 text-center">
+        <img src="https://fakeimg.pl/350x200/" alt="avatar" id="preview" class="img-thumbnail mb-2 mx-auto" />
+        <input type="file" id="image" class="form-control-file"/>
+      </div>
+      <div class="form-group py-2 d-grid">
+          <button type="submit" class="btn btn-primary">Enviar</button>
+      </div>
+    </form>
+  </template>
