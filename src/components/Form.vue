e9764a1d55961f63ee1d6c14616704fa5fa9c1d9
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-03-28 17:36:47 -0300

    Vinculamos la función handleFileChange con el elemento input file

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,7 +9,7 @@ function handleFileChange(event) {
     <form>
       <div class="form-group py-2 text-center">
         <img src="https://fakeimg.pl/350x200/" alt="avatar" id="preview" class="img-thumbnail mb-2 mx-auto" />
-        <input type="file" id="image" class="form-control-file"/>
+        <input type="file" id="image" class="form-control-file" @change="handleFileChange"/>
       </div>
       <div class="form-group py-2 d-grid">
           <button type="submit" class="btn btn-primary">Enviar</button>
