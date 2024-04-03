cb9d7abe0e6a99c33908ba746ef188f2d136a809
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-03 17:37:24 -0300

    Declaramos la función getUrlImage para actualizar el valor de url_image

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@ import Form from './components/Form.vue';
 import Viewer from './components/Viewer.vue';
 
 const url_image = ref("https://fakeimg.pl/600x300/");
+
+function getUrlImage(url) {
+  url_image.value = url;
+}
 </script>
 
 <template>
@@ -12,7 +16,7 @@ const url_image = ref("https://fakeimg.pl/600x300/");
       <div class="col-md-6">
         <h1 class="text-center display-3">Subir imagen</h1>
         <hr>
-        <Form />
+        <Form @getUrlImage="getUrlImage"/>
       </div>
     </div>
     <div class="row justify-content-center">
